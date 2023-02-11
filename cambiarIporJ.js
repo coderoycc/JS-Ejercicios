3fc1d956181b0a6c0775c5dbe454d92dfdd99919
coderoycc <rcchambi4@umsa.bo>
2023-02-11 16:43:51 -0400

    Cambia I x J sin bucles terminado

--- cambiarIporJ.js
+++ cambiarIporJ.js
@@ -7,19 +7,22 @@ const readline = require('readline').createInterface({
 })
 
 
-readline.question('Digite NUMERO Pos-I Pos-J\nEjemplo:\n172528 2 4\n', 
-(inp)=> {
-  let valores = inp.split(' ').map(x => parseInt(x))
-  const lon = parseInt(Math.log10(valores[0])+1)
-  const n = valores[0]
-  const i = valores[1]
-  const j = valores[2]
-  const digitoI = parseInt(n / Math.pow(10, lon - i)) % 10
-  const digitoJ = parseInt(n / Math.pow(10, lon - j)) % 10
-  
-  let nuevo = parseInt(n / Math.pow(10, lon - i)) - digitoI + digitoJ
-  console.log(digitoI, digitoJ, nuevo)
-  readline.close()
-})
+readline.question('Digite NUMERO Pos-I Pos-J\nEjemplo:\n172528 2 4\n',
+  (inp) => {
+    let valores = inp.split(' ').map(x => parseInt(x))
+    const lon = parseInt(Math.log10(valores[0]) + 1)
+    const n = valores[0]
+    const i = valores[1]
+    const j = valores[2]
+    const digitoI = parseInt(n / Math.pow(10, lon - i)) % 10
+    const digitoJ = parseInt(n / Math.pow(10, lon - j)) % 10
+
+    let nuevo = (parseInt(n / Math.pow(10, lon - i)) - digitoI + digitoJ) * Math.pow(10, lon - i) + (n % Math.pow(10, lon - i))
+    nuevo = (parseInt(nuevo / Math.pow(10, lon - j)) - digitoJ + digitoI) * Math.pow(10, lon - j) + (nuevo % Math.pow(10, lon - j))
+    console.log(`Cambiamos el digito ${digitoI} por el digito ${digitoJ}`)
+    console.log(`Original:\t ${n}`)
+    console.log(`Cambiado:\t ${nuevo}`)
+    readline.close()
+  })
 
 
---
+++ posibleSol.js
@@ -0,0 +1,7 @@
+let x = 234124
+parseInt(Math.log10(x)+1)
+x
+x
+x/10^1
+parseInt(x / Math.pow(10,6-6)) % 10
+(parseInt(x / Math.pow(10,6-3)) - 4 + 8 ) * Math.pow(10,6-3) + (x % Math.pow(10,6-3))
