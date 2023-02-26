768c133f0a40cb07ebe9aeefa12cfa7a51ff0771
coderoycc <rcchambi4@umsa.bo>
2023-02-25 22:40:27 -0400

    lectura-con-readline

--- leer.js
+++ leer.js
@@ -2,8 +2,9 @@ const readline = require("readline").createInterface({
     input: process.stdin,
     output: process.stdout
 })
-
+// Funcion recursiva como evento
 readline.on("line", (line) => {
     console.log(line.toUpperCase())
 })
+
 
