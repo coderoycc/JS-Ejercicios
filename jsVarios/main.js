f0d79f9a1d452b8bbb2c31699fbfc5d747c7ae2d
Roberto Chambi <rcchambi4@umsa.bo>
2023-06-18 18:29:38 -0400

    Cambio de paginas dinamicamente

--- jsVarios/main.js
+++ jsVarios/main.js
@@ -1,34 +1,76 @@
+let pagActual = 1;
+let datos = [];
+let paginas = 0;
 $(document).ready(() =>{
+  
   $.ajax({
     url: './main.php',
     type: 'GET',
     dataType: 'json',
     success: (data) => {
       console.log(data.success)
-      console.log(data.data)
+      datos = data.data;
+      console.log(datos)
       const cantidad = data.data.length;
       const maximo = 2;
-      const paginas = Math.ceil(cantidad / maximo);
+      paginas = Math.ceil(cantidad / maximo);
       let html = '';
       for (let i = 1; i <= paginas; i++) {
         html += `<li class="page-item" onclick="getActualPage(${i})"><a class="page-link" href="#">${i}</a></li>`;
       }
       html += `
-      <li class="page-item">
+      <li class="page-item" onclick="getNextPage()">
         <a class="page-link" href="#" aria-label="Next">
           <span aria-hidden="true">&raquo;</span>
-          <span class="sr-only">Next</span>
+          <span class="sr-only">Siguiente</span>
         </a>
       </li>`;
       $('#pagination').append(html);
       console.log(data.data.length)
+      mostrar();
+
     },
     error: (error) => {
       console.log(error)
     }
   })
+
+
 })
 
+function mostrar(){
+  let html = '';
+  let inicio = (pagActual - 1) * 2;
+  let final = inicio + 2;
+  final = (final > datos.length) ? datos.length : final;
+  for (let i = inicio; i < final; i++) {
+    html += `
+      <tr>
+        <td>${datos[i].nombre}</td>
+        <td>${datos[i].apellido}</td>
+        <td>${datos[i].edad}</td>
+      </tr>
+    `;
+  }
+  $('#tbody').append(html);
+
+}
+const getPreviousPage = () => {
+  if (pagActual > 1) {
+    pagActual--;
+    $('#tbody').html('');
+    mostrar();
+  }
+}
+const getNextPage = () => {
+  if (pagActual < paginas) {
+    pagActual++;
+    $('#tbody').html('');
+    mostrar();
+  }
+}
 const getActualPage = (page) => {
-  console.log("Actual click ", page)
+  pagActual = page;
+  $('#tbody').html('');
+  mostrar();
 }
